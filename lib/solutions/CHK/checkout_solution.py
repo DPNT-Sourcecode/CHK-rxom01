3c9c0d343b3c43ab834e6c11407182b24523c47a
sbx_user1051 <sbx_user1051@169.254.87.173>
2024-06-06 16:10:11 +0000

    Thu Jun 06 16:10:11 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,6 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    assert isinstance(skus, str)
+    return 0
+
