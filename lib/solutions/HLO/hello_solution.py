7be9e510df5749ffdb6b184a613b1131b433b783
sbx_user1051 <sbx_user1051@169.254.87.173>
2024-06-06 15:48:26 +0000

    Thu Jun 06 15:48:26 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,6 +2,7 @@
 # friend_name = unicode string
 def hello(friend_name: str) -> str:
     assert isinstance(friend_name, str)
-    return f"Hello {friend_name}"
+    return "Hello, World!"
+
 
 
--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -6,3 +6,7 @@ class TestHlo:
     def test_types_are_valid(self):
         with pytest.raises(AssertionError):
             assert hello_solution.hello(123) == "Hello 123"
+
+    def test_ignores_friend_name(self):
+        assert hello_solution.hello("Bob") == "Hello, World!"
+
