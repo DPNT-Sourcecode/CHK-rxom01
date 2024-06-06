46c2f86ac5a37988942b61568dc9571c147488fc
sbx_user1051 <sbx_user1051@169.254.87.173>
2024-06-06 16:13:11 +0000

    Thu Jun 06 16:13:11 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,14 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+ILLEGAL_INPUT: int = -1
+
+
 def checkout(skus: str) -> int:
-    assert isinstance(skus, str)
+    if not isinstance(skus, str):
+        return ILLEGAL_INPUT
+    if not all(c.isalpha() for c in skus):
+        return ILLEGAL_INPUT
     return 0
+
 
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -4,10 +4,12 @@ from solutions.CHK import checkout_solution
 
 class TestChk:
     def test_valid_param_types(self):
-        with pytest.raises(AssertionError):
-            assert checkout_solution.checkout(123) == 0
+        assert checkout_solution.checkout(123) == -1
 
     def test_input_is_all_alpha(self):
-        with pytest.raises(AssertionError):
-            assert checkout_solution.checkout("123") == 0
-            assert checkout_solution.checkout("+/'") == 0
+        assert checkout_solution.checkout("123") == -1
+        assert checkout_solution.checkout("+/'") == -1
+
+    def test_empty_string_returns_zero(self):
+        assert checkout_solution.checkout("") == 0
+
