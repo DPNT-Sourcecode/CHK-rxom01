d072c7776d9b98059b4cbbaeac7fb72b0432a2fe
sbx_user1051 <sbx_user1051@169.254.87.173>
2024-06-06 15:18:35 +0000

    Thu Jun 06 15:18:35 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,6 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x: int, y: int) -> int:
+    assert all(isinstance(param, int) for param in [x, y])
+    assert all(0 <= param <= 100 for param in [x, y])
+    return x + y
+
