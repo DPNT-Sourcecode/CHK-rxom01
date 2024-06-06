c22c6f03b3fee3efc5482afb18b0362b77cb37b9
sbx_user1051 <sbx_user1051@169.254.87.173>
2024-06-06 15:24:35 +0000

    Thu Jun 06 15:24:35 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,15 @@
+import pytest
 from solutions.SUM import sum_solution
 
 
-class TestSum():
+class TestSum:
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_raises_if_not_ints(self):
+        with pytest.raises(AssertionError):
+            assert sum_solution.compute(1.1, 0.9) == 2
+
+    def test_raises_if_out_of_range(self):
+        assert sum_solution.compute(-1, 0) == -1
+
